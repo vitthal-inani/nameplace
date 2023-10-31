2f2f3e547b79d93832b3c542547edf68e7f9f900
lohiyakeshav <keshav.lohiyas@gmail.com>
2023-10-31 22:28:30 +0530

    lobby

---
+++ lib/Screens/LobbyScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Lobby extends StatelessWidget {
+  const Lobby({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+
+      ),
+    );
+  }
+}
